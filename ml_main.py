4ce77438cd582452927f2b6ea940bc8402012e71
repeckadonatas <repeckadonatas@yahoo.comm>
2024-04-16 21:53:58 +0300

    minor updates

--- ml_main.py
+++ ml_main.py
@@ -1,6 +1,15 @@
-import ml_model.ml_model as mlm
+import source.ml_model as mlm
+import source.logger as log
+from source.constants import *
 
-model = mlm.Model(["XAUUSD", "XAGUSD", "XPTUSD", "XPDUSD"], 12, 1)
-model.train(use_generated_data=True)
-model.save("model1")
+ml_train_logger = log.app_logger(__name__)
+
+try:
+    model = mlm.Model(COMMODITIES, 12, 1)
+    for col in TRAINING_DATA_COLUMNS:
+        model.train(col)
+        model.save(f"model_{col}")
+        
+except Exception as e:
+    ml_train_logger.error('An error occured while training the model: {}'.format(e))
 
