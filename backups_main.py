9e31554a04f34b098320e3540d5c7e9b3c614493
repeckadonatas <repeckadonatas@yahoo.comm>
2024-04-18 20:38:03 +0300

    backup_functions: added backup_functions to source

---
+++ backups_main.py
@@ -0,0 +1,12 @@
+import source.backup_functions as bckp
+
+"""
+The main .py file to run backup functions from.
+"""
+
+try:
+    bckp.database_backup()
+    bckp.backup_ml_models()
+    bckp.remove_old_backups()
+except Exception as e:
+    raise e
